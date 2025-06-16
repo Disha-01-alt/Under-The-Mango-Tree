ad61b20823be67a142ee4c23bd066d14c627d0e6
Disha-01-alt <dishasahu786forstudy@gmail.com>
2025-06-16 11:30:05 +0530

    Create course_page.js

---
+++ static/js/course_page.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // --- Sidebar Toggle Logic ---
+    const sidebar = document.getElementById('course-sidebar');
+    const mainContent = document.getElementById('main-content');
+    const toggleButton = document.getElementById('sidebar-toggle');
+    const toggleIcon = document.getElementById('sidebar-toggle-icon');
+
+    if (sidebar && mainContent && toggleButton && toggleIcon) {
+        toggleButton.addEventListener('click', function() {
+            sidebar.classList.toggle('hidden');
+            const isSidebarHidden = sidebar.classList.contains('hidden');
+
+            if (isSidebarHidden) {
+                // EXPAND MAIN CONTENT
+                mainContent.classList.remove('lg:col-span-8', 'xl:col-span-9');
+                mainContent.classList.add('lg:col-span-12', 'xl:col-span-12');
+                toggleIcon.classList.remove('fa-angles-left');
+                toggleIcon.classList.add('fa-angles-right');
+            } else {
+                // SHRINK MAIN CONTENT
+                mainContent.classList.remove('lg:col-span-12', 'xl:col-span-12');
+                mainContent.classList.add('lg:col-span-8', 'xl:col-span-9');
+                toggleIcon.classList.remove('fa-angles-right');
+                toggleIcon.classList.add('fa-angles-left');
+            }
+        });
+    }
+});
